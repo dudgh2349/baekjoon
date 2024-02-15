1d86b64989fd0597100d36069ea219c8207ddfcf
user <user@DESKTOP-LPQU144>
2024-02-15 15:16:22 +0900

    "커밋 테스트2"

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,9 +1,14 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("test");
+		int a = sc.nextInt();
+		System.out.println(a + 8);
 	}
 
 }
